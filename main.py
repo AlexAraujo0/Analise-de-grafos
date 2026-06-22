10f80e5cb78f8b142bff71422ea4fc8361e833a4
AlexAraujo0 <alex.araujo0837@gmail.com>
2026-06-22 17:37:46 -0300

    Diminuição nos dados de amostra + adição de dica

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from app import iniciar
 def main():
     """Gera o grafo e inicializa a aplicação Dash."""
     grafo = Grafo()
-    G = grafo.gerar(2000, seed=4)
+    #Altere o primeiro parâmetro para gerar a quantidade de nós de pessoas que preferir, use a mesma seed se quiser reproduzir os dados.
+    G = grafo.gerar(800, seed=4)
     html_grafo = grafo.visualizar(html=True)
     
     iniciar(html_grafo, G)
